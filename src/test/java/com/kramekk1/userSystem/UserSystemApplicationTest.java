1ba1254108d0a34e2a1332155bb859e88c5f56f9
Jakub <kramekk1@gmail.com>
2025-03-23 22:07:56 +0100

    spring init

---
+++ src/test/java/com/kramekk1/userSystem/UserSystemApplicationTest.java
@@ -0,0 +1,13 @@
+package com.kramekk1.userSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserSystemApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
