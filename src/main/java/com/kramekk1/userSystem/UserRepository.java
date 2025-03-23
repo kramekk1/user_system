837eb2f0aa87bd8637c921c9ca1a37adae678bca
Jakub <kramekk1@gmail.com>
2025-03-23 23:32:21 +0100

    task 1

--- src/main/java/com/kramekk1/userSystem/UserRepository.java
+++ src/main/java/com/kramekk1/userSystem/UserRepository.java
@@ -1,7 +1,17 @@
 package com.kramekk1.userSystem;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Repository
+@RequiredArgsConstructor
 public class UserRepository {
+    private final List<User> userList;
+
+    public List<User> getAll() {
+        return new ArrayList<>(userList);
+    }
 }
